42dd0910d6b63cc19a91a31d8c4345dbec2a31d0
Matt McGee <mlmcgeenc@gmail.com>
2022-07-07 16:04:15 -0400

    POST works

--- prompt.js
+++ prompt.js
@@ -2,10 +2,12 @@ const cTable = require('console.table');
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 
+// QUERIES ========== ========== ========== ========= ==========
 function queryAllEmployees() {
 	return db
 		.promise()
-		.query(`SELECT
+		.query(
+			`SELECT
                 employeesTableA.id AS id,
                 employeesTableA.first_name AS first_name,
                 employeesTableA.last_name AS last_name,
@@ -19,17 +21,12 @@ function queryAllEmployees() {
                JOIN departments
                ON roles.department_id = departments.id
                LEFT JOIN employees AS employeesTableB
-               ON (employeesTableA.manager_id = employeesTableB.id)`)
+               ON (employeesTableA.manager_id = employeesTableB.id)`
+		)
 		.then((response) => {
-			result = cTable.getTable(response[0])
+			result = cTable.getTable(response[0]);
 			return result;
 		});
-}
-
-async function handleViewAllEmployees() {
-	console.log(`handleViewAllEmployees`);
-  table = await queryAllEmployees()
-  console.log(table)
 }
 
 function queryRolesList() {
@@ -44,21 +41,66 @@ function queryRolesList() {
 
 function queryEmployeesList() {
 	return db
-  .promise()
-  .query(`SELECT * FROM employees`)
-  .then((response) => {
-		array = response[0].map((element) => `${element.first_name} ${element.last_name}`);
-		return array;
-	});
+		.promise()
+		.query(`SELECT * FROM employees`)
+		.then((response) => {
+			array = response[0].map((element) => `${element.first_name} ${element.last_name}`);
+			return array;
+		});
 }
 
+function queryAllRoles() {
+	return db
+		.promise()
+		.query(
+			`SELECT roles.id AS role_id, roles.title AS role_title, roles.salary AS role_salary, departments.name AS department_name
+               FROM roles
+               JOIN departments
+               ON roles.department_id = departments.id`
+		)
+		.then((response) => {
+			result = cTable.getTable(response[0]);
+			return result;
+		});
+}
+
+function queryDepartmentsList() {
+	return db
+		.promise()
+		.query(`SELECT * FROM departments`)
+		.then((response) => {
+			array = response[0].map((element) => element.name);
+			return array;
+		});
+}
+
+function queryAllDepartments() {
+	return db
+		.promise()
+		.query(`SELECT * FROM departments`)
+		.then((response) => {
+			result = cTable.getTable(response[0]);
+			return result;
+		});
+}
+
+// ASYNC FUNCTIONS ========== ========== ========== ========= ==========
+// * converted
+async function handleViewAllEmployees() {
+	console.log(`handleViewAllEmployees`);
+	table = await queryAllEmployees();
+	console.log(table);
+}
+
+// * converted
+// TODO - add POST
 async function handleAddEmployee() {
 	console.log(`handleAddEmployee`);
 	let roles = [];
 	let managers = [];
 
 	roles = await queryRolesList();
-  managers = await queryEmployeesList();
+	managers = await queryEmployeesList();
 
 	inquirer
 		.prompt([
@@ -86,76 +128,78 @@ async function handleAddEmployee() {
 			},
 		])
 		.then((answer) => console.log(answer))
-    .then(() => chooseTask());
+		.then(() => chooseTask());
 }
 
+// TODO convert
+// TODO add POST
 handleUpdateEmployeeRole = () => {
 	console.log(`handleUpdateEmployeeRole`);
 };
 
+// * converted
 async function handleViewAllRoles() {
-	console.log(`handleViewAllRoles`);
-	const sql = `SELECT roles.id AS role_id, roles.title AS role_title, roles.salary AS role_salary, departments.name AS department_name
-               FROM roles
-               JOIN departments
-               ON roles.department_id = departments.id`;
-	db.query(sql, (err, rows) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		const table = cTable.getTable(rows);
-		console.log(`\n ${table} \n`);
-		return table;
-	});
+	console.log('handleViewAllRoles');
+	table = await queryAllRoles();
+	console.log(table);
 }
 
+function postRole(title, salary, department_id) {
+  const sql = `INSERT INTO roles (title, salary, department_id)
+               VALUES (?, ?, ?)`;
+	const params = [title, salary, department_id];
+  db.query(sql, params)
+}
+
+// * converted
+// TODO add POST
 async function handleAddRole() {
 	console.log(`handleAddRole`);
-	let choicesArray = [];
-	db.query(`SELECT * FROM departments`, (err, rows, fields) => {
-		const response = { rows };
-		choicesArray = response.rows.map((element) => element.name);
+	let departments = [];
 
-		inquirer
-			.prompt([
-				{
-					type: 'input',
-					name: 'roleName',
-					message: 'What is the name of the new role?',
-				},
-				{
-					type: 'input',
-					name: 'roleSalary',
-					message: 'What is the salary for the new role?',
-				},
-				{
-					type: 'list',
-					name: 'roleDepartment',
-					message: 'What department does the role belong to?',
-					choices: choicesArray,
-				},
-			])
-			.then((answer) => console.log(answer))
-			.then(() => chooseTask());
-	});
-	return;
+	departments = await queryDepartmentsList();
+
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'title',
+				message: 'What is the name of the new role?',
+			},
+			{
+				type: 'input',
+				name: 'salary',
+				message: 'What is the salary for the new role?',
+			},
+			{
+				type: 'list',
+				name: 'department',
+				message: 'What department does the role belong to?',
+				choices: departments,
+			},
+		])
+		.then((answer) => {
+      console.log(`title >>>`, answer.title);
+      console.log(`salary >>>`, answer.salary);
+      console.log(`department >>>`, answer.department);
+      console.log(departments)
+      const index = departments.findIndex((element) => element === answer.department)
+      department_id = index + 1
+      console.log(`department index `, index)
+      console.log(`department_id = `, department_id)
+      postRole(answer.title, answer.salary, department_id);
+    })
+		.then(() => chooseTask());
 }
 
+// * converted
 async function handleViewAllDepartments() {
 	console.log(`handleViewAllDepartments`);
-	const sql = `SELECT * FROM departments`;
-	db.query(sql, (err, rows) => {
-		if (err) {
-			console.log(err.message);
-			return;
-		}
-		const table = cTable.getTable(rows);
-		console.log('\n', table);
-		return table;
-	});
+	table = await queryAllDepartments();
+  console.log(table)
 }
 
+// TODO convert
 async function handleAddDepartment() {
 	console.log(`handleAddDepartment`);
 	inquirer
@@ -182,6 +226,7 @@ async function handleAddDepartment() {
 		.then(() => chooseTask());
 }
 
+// ROUTER ========== ========== ========== ========== ==========
 routeTask = (answer) => {
 	console.log(`Answer was:`, answer);
 	switch (answer) {
